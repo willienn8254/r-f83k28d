eea0f8473adb5d8f15c6a5357bf77d35adba43d0
welinton <willienn@hotmail.com>
2017-03-14 20:39:26 -0500

    Se agrega funcionalidad boton Incrementa

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+constructor(){
+
+	super();
+	this.state={numero: 0}
+
+
+}
+
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.numero}</span>
+        <button id="inc" onClick={this.incrementa.bind(this)}>Incrementa</button>
       </div>
     );
+  }
+
+
+  incrementa(){
+
+  	this.setState({
+
+  		numero: (this.state.numero+1)
+  	});
+
   }
 }
 
